63d40bc030e1cc9ad029f25548df6259fa4ae8c2
Tom Cheung <cheungch7@gmail.com>
2021-08-17 01:02:39 +0800

    Change http client to dio

--- lib/lihkg_webservices.dart
+++ lib/lihkg_webservices.dart
@@ -1,7 +1,10 @@
 import 'dart:convert';
 
-import 'package:http/http.dart' as http;
+import 'package:dio/dio.dart';
+import 'package:dio_cookie_manager/dio_cookie_manager.dart';
+import 'package:flutter/foundation.dart';
 import 'package:lihkg_flutter/model/thread_category.dart';
+import 'package:cookie_jar/cookie_jar.dart';
 
 import 'model/system_property.dart';
 import 'model/thread_content.dart';
@@ -19,9 +22,20 @@ extension PostOrderExtension on PostOrder {
   }
 }
 
+class LihkgHeaderInterceptor extends Interceptor {
+  final Map<String, String> _extraHeader;
+
+  LihkgHeaderInterceptor(this._extraHeader);
+
+  @override
+  void onRequest(RequestOptions options, RequestInterceptorHandler handler) {
+    options.headers.addAll(_extraHeader);
+    handler.next(options);
+  }
+}
+
 class LihkgWebServices {
-  static const String serverAPIRoot =
-      LihkgWebServices.serverRoot + "/api_v2/";
+  static const String serverAPIRoot = LihkgWebServices.serverRoot + "/api_v2";
   static const String serverRoot = "https://lihkg.com";
   static const Map<String, String> _header = {
     'User-Agent': 'LIHKG/3.10 iOS/14.5 iPhone/iPhone13,4',
@@ -31,22 +45,37 @@ class LihkgWebServices {
     'Host': 'lihkg.com'
   };
 
-  Future<SystemProperty> getSystemProperty() async {
-    final url = Uri.parse(serverAPIRoot + "system/property");
-    var response = await http.get(url, headers: _header);
-    var jsonResponse = jsonDecode(response.body);
+  static late Dio _dio = _getDio();
 
-    return SystemProperty.fromJson(jsonResponse['response']);
+  static Dio _getDio() {
+    var dio = Dio();
+    var cookieJar = CookieJar();
+    dio.interceptors.add(LihkgHeaderInterceptor(_header));
+    dio.interceptors.add(CookieManager(cookieJar));
+
+    if (kDebugMode) {
+      dio.interceptors
+          .add(LogInterceptor(requestBody: true, responseBody: false));
+    }
+    return dio;
+  }
+
+  Future<SystemProperty> getSystemProperty() async {
+    var response = await _dio.get("$serverAPIRoot/system/property");
+    return SystemProperty.fromJson(response.data['response']);
   }
 
   Future<ThreadCategory> getThreadCategory(
       {required String catId, int page = 1, int count = 30}) async {
-    final url = Uri.parse(serverAPIRoot +
-        "thread/category?cat_id=$catId&page=$page&count=$count&type=now");
-    var response = await http.get(url, headers: _header);
-    var jsonResponse = jsonDecode(response.body);
+    var response = await _dio.get("$serverAPIRoot/thread/category",
+        queryParameters: {
+          'cat_id': catId,
+          'page': page,
+          'count': count,
+          'type': 'now'
+        });
 
-    return ThreadCategory.fromJson(jsonResponse['response']);
+    return ThreadCategory.fromJson(response.data['response']);
   }
 
   Future<ThreadContentResponse> getThreadContent({
@@ -54,11 +83,8 @@ class LihkgWebServices {
     int page = 1,
     PostOrder order = PostOrder.ReplyTime,
   }) async {
-    final url = Uri.parse(serverAPIRoot +
-        "thread/$threadId/page/$page?order=${order.apiValue}");
-    var response = await http.get(url, headers: _header);
-    var jsonResponse = jsonDecode(response.body);
-
-    return ThreadContentResponse.fromJson(jsonResponse['response']);
+    var response = await _dio.get("$serverAPIRoot/thread/$threadId/page/$page",
+        queryParameters: {'order': order.apiValue});
+    return ThreadContentResponse.fromJson(response.data['response']);
   }
 }
